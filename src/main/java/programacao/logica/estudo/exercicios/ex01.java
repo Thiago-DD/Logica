76ba6c8f0ef5000528be23c0ed524d14e3e6d108
Thiago Rocha <thiagotlgdmn@gmail.com>
2025-06-03 00:19:33 -0300

    fix

--- src/main/java/programacao/logica/estudo/exercicios/ex01.java
+++ src/main/java/programacao/logica/estudo/exercicios/ex01.java
@@ -2,6 +2,21 @@ package programacao.logica.estudo.exercicios;
 
 public class ex01 {
     public static void main(String[] args) {
-        System.out.println("HelloWord");
+      /*
+      Faça um algoritmo que leia os valores de A, B, C e em seguida
+      imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+       */
+
+        int valorA = 30;
+        int valorB = 20;
+        int valorC = 60;
+        int result = valorA + valorB;
+
+        if (result < valorC) {
+            System.out.println("O valor de A e B é: " + result + ". " + result + " é menor que: " + valorC);
+        } else {
+            System.out.println(result);
+        }
+
     }
 }
